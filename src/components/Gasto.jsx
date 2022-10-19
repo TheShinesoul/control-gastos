ccdef5c624c93344b76fee4eb63e6a1ccd6b5157
TheShinesoul <jimcont.hector@gmail.com>
2022-10-19 02:42:40 -0500

    se agrega el formato que llevara cada gasto

---
+++ src/components/Gasto.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { formatFecha } from '../helpers/index'
+
+import IconoAhorro from '../img/icono_ahorro.svg'
+import IconoCasa from '../img/icono_casa.svg'
+import IconoComida from '../img/icono_comida.svg'
+import IconoGastos from '../img/icono_gastos.svg'
+import IconoOcio from '../img/icono_ocio.svg'
+import IconoSalud from '../img/icono_salud.svg'
+import IconoSuscripciones from '../img/icono_suscripciones.svg'
+
+const diccionarioIconos = {
+    ahorro: IconoAhorro,
+    comida: IconoComida,
+    casa: IconoCasa,
+    gastos: IconoGastos,
+    ocio: IconoOcio,
+    salud: IconoSalud,
+    subscripciones: IconoSuscripciones
+}
+
+
+
+const Gasto = ({ gasto }) => {
+    const { categoria, nombre, cantidad, id, fecha } = gasto
+    return (
+        <div className='gasto sombra'>
+            <div className='contenido-gasto'>
+                <img
+                    src={diccionarioIconos[categoria]}
+                    alt='Icono gastos'
+                />
+                <div className='descripcion-gasto'>
+                    <p className='categoria'>
+                        {categoria}
+                    </p>
+                    <p className='nombre-gasto'>
+                        {nombre}
+                    </p>
+                    <p className='fecha-gasto'>
+                        Agregado el: {''}
+                        <span>
+                            {formatFecha(fecha)}
+                        </span>
+                    </p>
+
+                </div>
+            </div>
+            <p className='cantidad-gasto'>
+                ${cantidad}
+            </p>
+        </div>
+    )
+}
+
+export default Gasto
